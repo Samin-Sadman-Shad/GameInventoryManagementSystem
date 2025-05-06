bb71db037a2d80e91216b3afc0c067374c184bb8
Samin-Sadman-Shad <saminsadmanshad@gmail.com>
2025-05-06 14:13:45 +0600

    inventory service created with dtos and entity

---
+++ src/Play.Inventory.Service/DTOs/Dtos.cs
@@ -0,0 +1,13 @@
+﻿namespace Play.Inventory.Service.DTOs
+{
+    public class Dtos
+    {
+        //grant/add items to the user
+        public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
+
+        //return the series of items a user possesses in the inventory
+        public record RetrieveAllInventoryItemDto(Guid UserId);
+
+        public record RetrieveInventoryItemByItemIdDto(Guid UserId, Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+    }
+}
---
+++ src/Play.Inventory.Service/Entities/InventoryItem.cs
@@ -0,0 +1,23 @@
+﻿using Play.Common.Abstraction;
+
+namespace Play.Inventory.Service.Entities
+{
+    public class InventoryItem : IBaseEntity
+    {
+        public Guid Id { get ; set ; }
+
+        /// <summary>
+        /// The Id of the user it belongs to
+        /// </summary>
+        public Guid UserId { get ; set ; }
+
+        /// <summary>
+        /// The Id of the Catalog item it belongs to
+        /// </summary>
+        public Guid CatalogId { get ; set ; }
+
+        public int Quantity { get ; set ; }
+
+        public DateTimeOffset DateAcquired { get ; set ; }
+    }
+}
